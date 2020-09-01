b749c72f480fb2d43ac00728b0b5bad0784296ac
raspy77 <shk9339@naver.com>
2020-09-01 19:05:33 +0900

    practice

--- practice02.py
+++ practice02.py
@@ -3,8 +3,20 @@
 a = int(input('수를 입력하세요'))
 # if (a/2) - (a/2) == 0:
 #     print('짝수')
+# else: a % 2 != 0:
+
 if a % 2 == 0:
     print('짝수')
 else:
     print('홀수')
-# else: a % 2 =! 0:
+
+#while
+while True:
+    i = input('정수를 입력하세요')
+    if i == 'quit':
+        break
+    if i.isdigit() is False:
+        break
+
+    i = int(i)
+    print('짝수' if i % 2 == 0 else '홀수')
